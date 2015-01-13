e3e4f087427190d0ae793c53621b020fce98db0c
Nichole Treadway <ntreadway@goodreads.com>
2015-01-12 20:13:28 -0800

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Treadway, Nichole on 1/12/15.
+//  Copyright (c) 2015 Treadway, Nichole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
